3d11cab7e1b65d53d162bdd9b6d8b40026019aee
Jheel Goel <mailjheel@gmail.com>
2022-07-27 20:53:02 -0700

    backend setup and completion

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import  products  from './data/products.js'
 
-
-const express = require('express')
-const products = require('./data/products')
 const app= express()
+dotenv.config()
 app.get('/', (req,res) => {
     res.send('API is running...')
 })
@@ -13,5 +14,6 @@ app.get('/api/products/:id', (req,res) => {
     const product = products.find((p) => p._id === req.params.id)
     res.json(product)
  })
- app.listen(5000, console.log('Server is running on port 5000'))
+ const port = process.env.Port || 5000
+ app.listen(5000, console.log(`Server is running in ${process.env.NODE_ENV} on port  ${port}`))
   
