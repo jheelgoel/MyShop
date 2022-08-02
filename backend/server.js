ba4bb7c73cfcb4f3f0a3fdf6ce567928383c2c30
Jheel Goel <mailjheel@gmail.com>
2022-08-01 20:03:26 -0700

    routing data

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import  products  from './data/products.js'
+//import  products  from './data/products.js'
 import connectDB from './config/db.js'
-import User from './models/userModel.js'
-import Product from './models/productModel.js'
+//import User from './models/userModel.js'
+//import Product from './models/productModel.js'
+import productRoutes from './routes/productRoutes.js'
 const app= express()
 dotenv.config()
 connectDB()
 app.get('/', (req,res) => {
     res.send('API is running...')
 })
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
- })
+app.use('/api/products', productRoutes)
+
  const port = process.env.Port || 5000
- app.listen(5000, console.log(`Server is running in ${process.env.NODE_ENV} on port  ${port}`))
+ app.listen(port, console.log(`Server is running in ${process.env.NODE_ENV} on port  ${port}`))
   
